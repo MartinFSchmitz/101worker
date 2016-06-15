0b504489dbe462f6f6aabcf3a74f53989f2b3cb0
Felix Schröder <fschroeder@uni-koblenz.de>
2016-06-16 00:22:08 +0200

    Tokenizing works for all contributions, corresponding dump file will be created

--- modules/featureLocation/__init__.py
+++ modules/featureLocation/__init__.py
@@ -10,21 +10,47 @@ config = {
     'wantsfiles': True,
     'threadsafe': True,
     'behavior': {
-        'creates': [['resource', 'fl']]
+        'creates': [['dump', 'featureLocation']]
     }
 }
 
-# this is the actual logic of the module
-
 def update_file(context, f):
-    # reads the content of the file (primary resource)
     try:
+        # read the content of the file (primary resource)
         source = context.get_primary_resource(f)
-        tokens = remove_stopwords(process_text(source))
+        # tokenize the source file (stems without stopwords)
+        sourceTokens = process_text(source)
 
-        context.write_derived_resource(f, tokens, 'fl')
+        # read possible feature tokens
+        featureTokens = get_feature_tokens()
+
+        # calculate intersection between actual and possible tokens
+        s_f_token_intersect = list(set(sourceTokens) & set(featureTokens))
+
+        # read existing feature locations
+        data = context.read_dump('featureLocation')
+
+        # init data if empty
+        if data is None:
+            data = {}
+
+        # get name of current contribution
+        if f.startswith('contributions' + os.sep):
+            contribution = f.split(os.sep)[1]
+            
+            # init new contribution if missing
+            if data.get(contribution, None) is None:
+                data[contribution] = []
+
+            # calculate union of existing and new tokens
+            data[contribution] = list(set(data[contribution]) | set(s_f_token_intersect))
+
+        # save dump file
+        context.write_dump('featureLocation', data)
+
     except UnicodeDecodeError:
-        context.write_derived_resource(f, 0, 'fl')
+        context.write_dump('featureLocation', 0)
+        print("Error occured while executing featureLocation for file " + f)
 
 def remove_file(context, f):
     context.remove_derived_resource(f, 'fl')
@@ -40,13 +66,15 @@ def run(context, change):
     else:
         remove_file(context, change['file'])
 
-remove_punctuation_map = dict((ord(char), None) for char in string.punctuation)
+
 
 def process_text(text):
+    # stem and tokenize text
+    remove_punctuation_map = dict((ord(char), None) for char in string.punctuation)
     text = text.lower().translate(remove_punctuation_map)
     tokens = nltk.word_tokenize(text)
     stemmer = PorterStemmer()
-    tokens = [stemmer.stem(t) for t in tokens]
+    tokens = remove_stopwords([stemmer.stem(t) for t in tokens])
     return tokens
  
 def remove_stopwords(tokens):
@@ -54,3 +82,8 @@ def remove_stopwords(tokens):
     content = [w for w in tokens if w not in stopwords]
     return content
 
+def get_feature_tokens():
+    dir = os.path.dirname(__file__)
+    with open(dir + os.sep + 'featureTokens.json') as data_file:    
+        data = json.load(data_file)
+    return data
