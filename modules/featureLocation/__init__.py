a6f394b944eae7de9083c3a516f9b3bfd44a057b
Felix Schröder <fschroeder@uni-koblenz.de>
2016-06-16 10:35:18 +0200

    Try to fix error with [] in json dump file

--- modules/featureLocation/__init__.py
+++ modules/featureLocation/__init__.py
@@ -49,7 +49,7 @@ def update_file(context, f):
         context.write_dump('featureLocation', data)
 
     except UnicodeDecodeError:
-        context.write_dump('featureLocation', [])
+        context.write_dump('featureLocation', {[]})
         print("Error occured while executing featureLocation for file " + f)
 
 
